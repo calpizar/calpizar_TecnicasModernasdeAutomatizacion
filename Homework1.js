069a078deff70db5f82cb0acb9a5837e9628f66c
calpizar02 <carmen.alpizartrigueros@fiserv.com>
2021-09-30 14:03:07 -0600

    Homework 1 - Complete

--- Homework1.js
+++ Homework1.js
@@ -16,6 +16,43 @@ function flat(data) {
 }
 console.log (flat(arrays));
 
+//Ejercicio 3
+function compoundMatch(words,target){
+
+  const array = ["bow", "crystal","organic","ally","rain","line"];
+
+  for (let i = 0; i <words.length; i++){
+      if(target.includes(words[i])){
+          arr1.push(words[i]);
+
+      }
+  }
+
+  let part1;
+  let part2;
+
+  outer: for (let i = 0; i < arr.length; i++){
+
+      if (target.includes(arr1[i])) {
+          part1 = array[i];
+          for (let j = 0; j < arr1.length; j++){
+              if (target.replace(part1,) ===arr1[j]){
+                  part2 = arr1[j];
+                  break outer;
+              }
+          }
+      }
+}
+
+if (part1 === undefined || part2 === undefined) {
+  return null;
+
+  }
+  let indexes = part1 + part2 === target ? 
+  [words.indexOf(part1), words.indexOf(part2)] : [words.indexOf(part2), words.indexOf(part1)];
+  console.log([part1, part2, indexes]);
+}
+compoundMatch(["bow", "crystal","organic","ally","rain","line"]);
 
 //Ejercicio #4 (Elever cada digito de un numero al cuadrado y concatenar el resultado)
 function squareDigits(n) {
